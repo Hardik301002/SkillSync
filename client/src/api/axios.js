83fa6664899e3a49cb74987c8b29dcaa74f23f93
Hardik301002 <hardikgupta769@gmail.com>
2026-02-03 12:27:03 +0530

    Updated API URL for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api/v1' });
+// Automatically detect if we are on localhost or deployed
+const isDevelopment = import.meta.env.MODE === 'development';
+
+const API = axios.create({
+    baseURL: isDevelopment 
+        ? 'http://localhost:5000/api/v1' 
+        : 'https://skillsync-1ppr.onrender.com/api/v1' 
+});
 
 API.interceptors.request.use((req) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        req.headers.Authorization = `Bearer ${token}`;
+    if (localStorage.getItem('token')) {
+        req.headers['x-auth-token'] = localStorage.getItem('token');
     }
     return req;
 });
